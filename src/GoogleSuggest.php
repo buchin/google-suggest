d2ee9bb2cc8d87147174764681702fe2f77886ed
Mochammad Masbuchin <buchin@dropsugar.com>
2018-10-14 22:03:00 +0700

    feature: language, country, and source support

--- src/GoogleSuggest.php
+++ src/GoogleSuggest.php
@@ -6,9 +6,30 @@
 class GoogleSuggest
 {
 	
-	public static function grab($keyword = '')
+	public static function grab($keyword = '', $lang = '', $country = '', $source = '')
 	{
-		if($content = trim(file_get_contents('http://google.com/complete/search?output=toolbar&q='.urlencode($keyword))))
+        $url = 'http://google.com/complete/search?';
+
+        $query = [
+            'output' => 'toolbar',
+            'q' => $keyword,
+        ];
+
+        if(!empty($lang)){
+            $query['hl'] = $lang;
+        }
+
+        if(!empty($country)){
+            $query['gl'] = $country;
+        }
+
+        if(!empty($source)){
+            $query['ds'] = $source;
+        }
+
+        $url .= http_build_query($query);
+
+		if($content = trim(file_get_contents($url)));
         {
             $xml = simplexml_load_string(utf8_encode($content));
             
